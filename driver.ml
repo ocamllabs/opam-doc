7666b854b9ea84655d056a95971bad2adef4419e
Leo White <lpw25@cl.cam.ac.uk>
2013-02-27 17:03:39 +0000

    Print json prettier

--- driver.ml
+++ driver.ml
@@ -1,5 +1,6 @@
 open Index
 open Generate
+open Cow
 
 module StringMap = Map.Make(String)
 
@@ -11,6 +12,58 @@ let get_cmt cmd cmt_list =
     try
       List.find pSameBase cmt_list
     with Not_found -> raise (Failure ("Missing cmt file: " ^ cmd))
+
+let rec list_iter_between f o = function
+  | [] -> ()
+  | [h] -> f h
+  | h::t -> f h; o (); list_iter_between f o t
+
+let escape_string s =
+  let buf = Buffer.create 80 in
+    Buffer.add_string buf "\"";
+    for i = 0 to String.length s - 1
+    do
+      let x =
+        match s.[i] with
+        | '\n' -> "\\n"
+        | '\t' -> "\\t"
+        | '\r' -> "\\r"
+        | '\b' -> "\\b"
+        | '\\' -> "\\\\"
+        | '/' -> "\\/"
+        | '"' -> "\\\""
+        | '\x0c' -> "\\f"
+        | c -> String.make 1 c
+      in
+        Buffer.add_string buf x
+    done;
+    Buffer.add_string buf "\"";
+    Buffer.contents buf
+
+let rec to_fct t f =
+  match t with
+  | Json.Int i -> f (Printf.sprintf "%Ld" i)
+  | Json.Bool b -> f (string_of_bool b)
+  | Json.Float r -> f (Printf.sprintf "%g" r)
+  | Json.String s -> f (escape_string s)
+  | Json.Null -> f "null"
+  | Json.Array a ->
+    f "[";
+    list_iter_between (fun i -> to_fct i f) (fun () -> f ", \n") a;
+    f "\n]";
+  | Json.Object a ->
+    f "{";
+    list_iter_between (fun (k, v) -> to_fct (Json.String k) f; f ": "; to_fct v f)
+      (fun () -> f ", \n") a;
+    f "\n}"
+
+let json_to_buffer t buf =
+  to_fct t (fun s -> Buffer.add_string buf s)
+
+let json_to_string json = 
+  let buf = Buffer.create 2048 in
+    json_to_buffer json buf;
+    Buffer.contents buf
 
 let process_file global cmd cmt = 
   let dintf = 
@@ -35,7 +88,7 @@ let process_file global cmd cmt =
     let json = Docjson.json_of_file jintf in
     let json_name = (Filename.chop_extension cmd) ^ ".json" in
     let oc = open_out json_name in
-      output_string oc (Cow.Json.to_string json);
+      output_string oc (json_to_string json);
       close_out oc
 
 let _ = 
