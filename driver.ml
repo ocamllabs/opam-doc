55710bf225dd7b96fb0e7ec8d5c0a62e161af316
Anil Madhavapeddy <anil@recoil.org>
2013-08-28 17:11:41 +0100

    whitespace

--- driver.ml
+++ driver.ml
@@ -9,7 +9,7 @@ let (>>) h f = f h
 let create_package_directory () =
   let package_name = !Opam_doc_config.current_package in
   if not Sys.(file_exists package_name && is_directory package_name) then
-    Unix.mkdir package_name 0o755  
+    Unix.mkdir package_name 0o755
 
 let get_cmt cmd cmt_list =
   let base = Filename.chop_extension cmd in
@@ -17,33 +17,33 @@ let get_cmt cmd cmt_list =
     try
       List.find pSameBase cmt_list
     with Not_found -> raise (Failure ("Missing cmt file: " ^ cmd))
-    
+
 let process_cmd cmd =
   Cmd_format.(
-    try 
-      let cmd = read_cmd cmd in Some cmd.cmd_doctree 
+    try
+      let cmd = read_cmd cmd in Some cmd.cmd_doctree
     with
-	_ -> None			     
+	_ -> None
   )
 
 let check_package_name_conflict global =
-  let rec loop () = 
+  let rec loop () =
     Printf.printf "Package '%s' already exists. Proceed anyway? [Y/n/r] \n%!"
       !Opam_doc_config.current_package;
     Scanf.scanf "%c" (function
       | 'Y' | '\n' -> false
       | 'n' -> Printf.printf "Conflict unresolved. Exiting now..."; exit 0
-      | 'r' -> 
+      | 'r' ->
 	Printf.printf "New package name : ";
 	Opam_doc_config.current_package := read_line ();
 	false
       | _ -> loop ())
   in
-  while Index.package_exists global !Opam_doc_config.current_package && 
+  while Index.package_exists global !Opam_doc_config.current_package &&
     not !Opam_doc_config.always_proceed && loop () do () done
 
-let process_file global cmd cmt = 
-  let module_name = String.capitalize 
+let process_file global cmd cmt =
+  let module_name = String.capitalize
     (Filename.chop_extension (Filename.basename cmd)) in
   let doctree = process_cmd cmd in
   let cmi, cmt = Cmt_format.read cmt in
@@ -54,74 +54,74 @@ let process_file global cmd cmt =
       let imports = cmi.Cmi_format.cmi_crcs in
       let local = create_local global imports in
       Index.reset_internal_reference_table ();
-      try 
+      try
 	match cmt.Cmt_format.cmt_annots with
-          | Cmt_format.Interface intf ->   
+          | Cmt_format.Interface intf ->
 	    Some (generate_file_from_interface local module_name doctree intf)
-          | Cmt_format.Implementation impl ->  
+          | Cmt_format.Implementation impl ->
 	    Some (generate_file_from_structure local module_name doctree impl)
-          | _ -> raise (Failure "Wrong kind of cmt file") 
+          | _ -> raise (Failure "Wrong kind of cmt file")
       with
 	| Invalid_argument s ->
 	  Printf.eprintf "Error \"%s\" while processing module %s. File skipped\n%!" 
 	    s module_name;
 	  None
-	    
+
 let _ =
   let files = ref [] in
-  
+
   Opam_doc_config.(
     Arg.parse options (fun file -> files := file :: !files) usage
   );
 
-  (* read the saved global table *)  
+  (* read the saved global table *)
   let global = read_global_file !Opam_doc_config.index_file_path in
-  
+
   check_package_name_conflict global;
 
-  let global = add_global_package global 
-    !Opam_doc_config.current_package 
+  let global = add_global_package global
+    !Opam_doc_config.current_package
     !Opam_doc_config.package_descr in
 
   let cmt_files = List.filter
     (fun file -> Filename.check_suffix file ".cmti"
       || Filename.check_suffix file ".cmt") !files in
-  
-  let cmd_files = List.filter 
-    (fun file -> Filename.check_suffix file ".cmdi" 
+
+  let cmd_files = List.filter
+    (fun file -> Filename.check_suffix file ".cmdi"
       || Filename.check_suffix file ".cmd") !files in
 
   (* Remove the [ext] file when a [ext]i is found *)
-  let filter_impl_files ext files = 
+  let filter_impl_files ext files =
     let should_be_kept file =
       if Filename.check_suffix file ext then
-	try 
-	  ignore (List.find ((=) ((Filename.chop_extension file)^ext^"i")) files); 
-	  false 
+	try
+	  ignore (List.find ((=) ((Filename.chop_extension file)^ext^"i")) files);
+	  false
 	with Not_found -> true
       else true
 	in
-    List.filter should_be_kept files 
+    List.filter should_be_kept files
   in
 
-  let cmt_files = filter_impl_files ".cmt" cmt_files in  
-  let cmd_files = filter_impl_files ".cmd" cmd_files in 
+  let cmt_files = filter_impl_files ".cmt" cmt_files in
+  let cmd_files = filter_impl_files ".cmd" cmd_files in
 
-  (* Update the global table with the future processed cmts *)  
+  (* Update the global table with the future processed cmts *)
   let global = update_global global cmt_files in
-  
+
   create_package_directory ();
-      
+
   let processed_files =
-    List.map 
+    List.map
       (fun cmd -> let cmt = get_cmt cmd cmt_files in
 		  try process_file global cmd cmt with e -> raise e)
-      cmd_files 
+      cmd_files
     >> List.filter (function Some o -> true | None -> false)
     >> List.map (function Some o -> o | None -> assert false)
     >> List.sort (fun (x,_) (y,_) -> compare x y)
   in
-  
+
   if processed_files != [] then
     begin
       let open Html_utils in
--- index.ml
+++ index.ml
@@ -32,16 +32,16 @@ type global = { map: t_value CrcMap.t
 	      ; package_list: package_list }
 
 type local = t_value LocalMap.t
-    
+
 let read_global_file path =
-  if !Opam_doc_config.clear_index then 
+  if !Opam_doc_config.clear_index then
     {map= CrcMap.empty; package_list= []}
   else
-    try 
+    try
       let ic = open_in path in
       let global = input_value ic in
       close_in ic;
-      global 
+      global
     with
       | Sys_error _ -> {map= CrcMap.empty; package_list= []}
 
@@ -50,23 +50,23 @@ let update_global global filenames =
     let cmio, cmto = Cmt_format.read fname in
     match cmio, cmto with
       (* looking up for a packed cmt *)
-      | Some cmi, Some cmt -> 
+      | Some cmi, Some cmt ->
 	let module_name = cmi.Cmi_format.cmi_name in
-	let (name,crc) = 
+	let (name,crc) =
 	  match cmi.Cmi_format.cmi_crcs with
-	    | e::l -> 
+	    | e::l ->
 	      e
 	    | _ -> assert false
 	in
-	begin 
-	  match cmt.Cmt_format.cmt_annots with 
+	begin
+	  match cmt.Cmt_format.cmt_annots with
 	    (* If this is a packed module, we need a way to find the path to submodules *)
-	    | Cmt_format.Packed _ -> 
-	      (* The references should be present in the table after the update *)      
+	    | Cmt_format.Packed _ ->
+	      (* The references should be present in the table after the update *)
 	      CrcMap.add
 		(module_name, crc)
 		(* Filter the self-references *)
-		(Packed_module 
+		(Packed_module
 		    (!Opam_doc_config.current_package,
 		     (List.filter ((<>) (name,crc)) cmi.Cmi_format.cmi_crcs)))
 		acc
@@ -78,22 +78,21 @@ let update_global global filenames =
 	    | _ -> acc (* shouldn't happen but you never know :l *)
 	end
       | _ ->
-	Printf.eprintf "Index.update_global: Wrong cmt file handed\n%!";
+	Printf.eprintf "Index.update_global: %s -- Wrong cmt file handed\n%!" fname;
 	acc
   in
   let newmap = List.fold_left doFile global.map filenames in
   { global with map = newmap }
 
-    
 let write_global_file global path =
   let oc = open_out path in
   output_value oc global;
   close_out oc
-    
+
 (* debug *)
 let global_print global =
   CrcMap.iter
-    (fun (x,y) z -> 
+    (fun (x,y) z ->
       Printf.printf "key : (%s,%s) - value: %s\n" x (Digest.to_hex y)
 	(match z with
 	    Packed_module _ -> "Packed"
@@ -105,13 +104,13 @@ let global_find_key (global:global) key =
   CrcMap.iter
     (fun (x,y) z ->
       if x = key then
-	Printf.printf "key : (%s,%s) - value: %s\n" x (Digest.to_hex y) 
+	Printf.printf "key : (%s,%s) - value: %s\n" x (Digest.to_hex y)
 	  (match z with
 	      Packed_module (_, sub_modules) -> "Packed\n"^
-		(List.fold_left 
-		   (fun acc (x,y) -> 
+		(List.fold_left
+		   (fun acc (x,y) ->
 		     acc^"\t Modname:"^x^" - Digest : "
-		     ^(Digest.to_hex y)^"\n") 
+		     ^(Digest.to_hex y)^"\n")
 		   ""
 		   sub_modules)
 	    | Direct_path (_, str) -> "Direct path : "^str))
@@ -121,12 +120,12 @@ let local_print table = ()
 
 let global_lookup global md = CrcMap.find md global.map
 
-let create_local global mds = 
+let create_local global mds =
   let rec doMod pack acc ((name, _) as md)  =
     try
       let value = CrcMap.find md global.map in
       match value with
-	| Packed_module (_,crcs) -> 
+	| Packed_module (_,crcs) ->
 	  let acc = List.fold_left (doMod (Some name)) acc crcs in
 	  LocalMap.add (pack, name) value acc
 	| Direct_path _ ->
@@ -139,12 +138,12 @@ let is_module name =
   name.[0] >= 'A' && name.[0] <= 'Z'
 
 (* assuming that if a type name is present, it is at the end of the list *)
-let rec assemble_path is_class (pack, path) = function 
+let rec assemble_path is_class (pack, path) = function
   | [] -> "?package="^pack^"&module="^path
   | h::[] ->
     if is_module h then
       "?package="^pack^"&module="^path^"."^h
-    else 
+    else
       "?package="^pack^"&module="^path^(if is_class then "&class=" else "&type=")^h
   | h::t ->
     if not (is_module h) then raise (Failure "Incorrect signature");
@@ -156,7 +155,7 @@ let local_lookup local ?(is_class=false) path_elems =
       begin
 	match LocalMap.find (pack, m1) local with
 	  | Direct_path (pack, str) -> (pack,str), (m2::r)
-	  | Packed_module _ -> 
+	  | Packed_module _ ->
 	    loop (Some m1) (m2::r)
       end
     | m1::[] ->
@@ -165,7 +164,7 @@ let local_lookup local ?(is_class=false) path_elems =
 	| Packed_module _ -> raise Not_found
 	| Direct_path (pack, str) -> (pack, str), []
       end
-    | [] -> raise Not_found    
+    | [] -> raise Not_found
   in
   let ((package,path), rest) = loop None path_elems in
   assemble_path is_class (package,path) rest
@@ -174,7 +173,7 @@ let get_global_packages global =
   global.package_list
 
 let package_exists global package_name =
-  try 
+  try
     ignore (List.find (fun (n,_) -> n = package_name) global.package_list);
     true
   with Not_found -> false
@@ -183,20 +182,20 @@ let add_global_package global package_name info =
   let info_opt = if info = "" then None else Some (Cow.Html.of_string info) in
   let rec add_or_replace acc = function
     | [] -> List.rev ((package_name, info_opt)::acc)
-    | (h,_)::t when h = package_name -> 
+    | (h,_)::t when h = package_name ->
       (package_name, info_opt)::acc@t
     | h::t -> add_or_replace (h::acc) t
   in
   {global with package_list = (add_or_replace [] global.package_list)}
 
 (* Internal references part *)
-open Hashtbl 
+open Hashtbl
 
 let internal_table = create 32
 
-let reset_internal_reference_table () = 
+let reset_internal_reference_table () =
   reset internal_table
-    
+
 let add_internal_reference = add internal_table
-   
+
 let lookup_internal_reference = find internal_table
