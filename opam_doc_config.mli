65454ef83025e9ba2f070ada3dcc6f3b88691783
Jeremy Yallop <yallop@gmail.com>
2013-08-31 11:58:48 +0100

    Interface file for Opam_doc_config.

---
+++ opam_doc_config.mli
@@ -0,0 +1,26 @@
+val index_file_path : string ref
+val default_index_name : string ref
+val filter_pervasives : bool ref
+val clear_index : bool ref
+val always_proceed : bool ref
+val package_descr : string ref
+val current_package : string ref
+val options : (string * Arg.spec * string) list
+val usage : string
+val doctype : string
+val character_encoding : Cow.Xml.t
+val default_stylesheet : string list
+type mark =
+    Attribute
+  | Type
+  | Type_elt
+  | Function
+  | Exception
+  | Value
+  | Method
+  | Title
+val style_filename : string
+val style_tag : Cow.Xml.t
+val script_filename : string
+val script_tag : Cow.Xml.t
+val default_script : string
