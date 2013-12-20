9113c7a931dbe862d98bfe11f276c7a15c3dc6f5
Leo White <lpw25@cl.cam.ac.uk>
2013-12-20 12:15:42 +0000

    Improve the debug output from printdoctree

--- src/bin-doc/printdoctree.mli
+++ src/bin-doc/printdoctree.mli
@@ -1,10 +1,12 @@
 open Format
 open Doctree
 
+val module_type : int -> formatter -> module_type -> unit
 val signature :
   int -> formatter -> signature_item list -> unit
 val signature_item :
   int -> formatter -> signature_item -> unit
+val module_expr : int -> formatter -> module_expr -> unit
 val structure : int -> formatter -> structure -> unit
 val structure_item : int -> formatter -> structure_item -> unit
 val interface : int -> formatter -> interface -> unit
