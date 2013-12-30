f47c6e9b66bc7a06e3028a78fca28f9387074e72
Anil Madhavapeddy <anil@recoil.org>
2013-12-30 01:12:16 +0000

    `opam doc-serve` will default to the OPAM root now

--- scripts/opam-doc-serve.sh
+++ scripts/opam-doc-serve.sh
@@ -1,7 +1,12 @@
-#!/bin/sh -e
+#!/usr/bin/env bash
 # Serve the doc contents
 
-cd $1
+DOC="$1"
 
+if [ $DOC = "" ]; then
+  DOC=$(opam config var root)/doc/doc
+fi
+
+cd $DOC
 cohttp-server -p 8000 || true
 python -m SimpleHTTPServer 
